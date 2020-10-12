d11f5a98b12b05f5ab57d150af352b4a6c826a69
Scott Johnson <c.scott.johnson@gmail.com>
2020-10-11 23:18:10 -0500

    Set up basic file structure

---
+++ index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { ThemeProvider } from './contexts/theme'
+import Loading from './components/Loading'
+import Nav from './components/Nav'
+
+const Posts = React.lazy(() => import('./components/Posts'))
+const Post = React.lazy(() => import('./components/Post'))
+const User = React.lazy(() => import('./components/User'))
+
+class App extends React.Component {
+  state = {
+    theme: 'light',
+    toggleTheme: () => {
+      this.setState(({ theme }) => ({
+        theme: theme === 'light' ? 'dark' : 'light'
+      }))
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className='container'>
+              <Nav />
+
+              <React.Suspense fallback={<Loading />}>
+                <Switch>
+                 
+                </Switch>
+              </React.Suspense>
+            </div>
+          </div>
+        </ThemeProvider>
+      </Router>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
